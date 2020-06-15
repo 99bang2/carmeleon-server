7fea8ffab093094ffe7d11ad487e3a999422152c
Jonghyun <jonghyun7147@gmail.com>
2020-06-15 15:42:01 +0900

    admin, notice, event CRUD API 업데이트

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -1,17 +1,29 @@
 'use strict'
-const response = require('../libs/response')
 const Router = require('koa-router')
-const codes = require('../configs/codes.json')
 const api = new Router()
-const exampleController = require('../controllers/example')
+const adminController = require('../controllers/admin')
+const noticeController = require('../controllers/admin')
+const eventController = require('../controllers/admin')
 
-/**
- * example
- */
-api.post('/complexes', exampleController.create)
-api.get('/complexes', exampleController.list)
-api.get('/complexes/:uid', exampleController.read)
-api.put('/complexes/:uid', exampleController.update)
-api.delete('/complexes/:uid', exampleController.delete)
+/*
+* Carmelon Sever
+* */
+api.post('/admins', adminController.create)
+api.get('/admins', adminController.list)
+api.get('/admins/:uid', adminController.read)
+api.put('/admins/:uid', adminController.update)
+api.delete('/admins/:uid', adminController.delete)
+
+api.post('/notices', noticeController.create)
+api.get('/notices', noticeController.list)
+api.get('/notices/:uid', noticeController.read)
+api.put('/notices/:uid', noticeController.update)
+api.delete('/notices/:uid', noticeController.delete)
+
+api.post('/events', eventController.create)
+api.get('/events', eventController.list)
+api.get('/events/:uid', eventController.read)
+api.put('/events/:uid', eventController.update)
+api.delete('/events/:uid', eventController.delete)
 
 module.exports = api
