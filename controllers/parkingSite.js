5d37ff4dc6b780e516f38294d13bb2e9494cfa34
Jonghyun <jonghyun7147@gmail.com>
2020-06-25 18:54:50 +0900

    parkingSite Controller create modify

--- controllers/parkingSite.js
+++ controllers/parkingSite.js
@@ -1,18 +1,8 @@
 const models = require('../models')
 const response = require('../libs/response')
-const imageUpload = require('../libs/imageUpload')
-const common = require('../libs/common')
-const dir = './uploads/site/'
-const folder = 'site/'
 
 exports.create = async function (ctx) {
     let _ = ctx.request.body
-	_.paymentTag = common.makeArray(_.paymentTag)
-	_.brandTag = common.makeArray(_.brandTag)
-	_.productTag = common.makeArray(_.productTag)
-	_.optionTag = common.makeArray(_.optionTag)
-	_.carTag = common.makeArray(_.carTag)
-	_.picture = common.makeArray(_.picture)
     let parkingSite = await models.parkingSite.create(_)
     response.send(ctx, parkingSite)
 }
@@ -55,8 +45,8 @@ exports.bulkDelete = async function (ctx) {
     response.send(ctx, deleteResult)
 }
 
-exports.searchList = async function (ctx) {
-	let _ = ctx.request.query
-	let parkingSite = await models.parkingSite.search(_, models)
-	response.send(ctx, parkingSite)
-}
+// exports.searchList = async function (ctx) {
+// 	let _ = ctx.request.query
+// 	let parkingSite = await models.parkingSite.search(_, models)
+// 	response.send(ctx, parkingSite)
+// }
