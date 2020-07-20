348a7ad8319922a6aef428b18d12bae850f5affe
Jonghyun <jonghyun7147@gmail.com>
2020-07-20 12:30:50 +0900

    evCharge Model Update

---
+++ models/evCgarge.js
@@ -0,0 +1,116 @@
+'use strict'
+const response = require('../libs/response')
+const codes = require('../configs/codes.json')
+
+module.exports = (sequelize, DataTypes) => {
+	const evCharge = sequelize.define('evCharge', {
+		uid: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		statName: {
+			type: DataTypes.STRING,
+		},
+		statId: {
+			type:DataTypes.STRING
+		},
+		chgerId: {
+			type: DataTypes.STRING
+		},
+		chgerType: {
+			type: DataTypes.STRING,
+		},
+		addr: {
+			type: DataTypes.STRING
+		},
+		useTime: {
+			type: DataTypes.STRING
+		},
+		busiId: {
+			type: DataTypes.STRING,
+		},
+		busiNm: {
+			type: DataTypes.STRING,
+		},
+		busiCall: {
+			type: DataTypes.STRING,
+		},
+		statUpdDt: {
+			type: DataTypes.STRING
+		},
+		powerType: {
+			type: DataTypes.STRING
+		},
+		rate: {
+			type: DataTypes.DOUBLE
+		},
+		lat: {
+			type:DataTypes.DOUBLE
+		},
+		lon: {
+			type:DataTypes.DOUBLE
+		},
+		sido: {
+			type: DataTypes.STRING
+		},
+		sigungu:{
+			type: DataTypes.STRING
+		},
+	}, {
+		timestamps: true,
+		paranoid: true,
+		underscored: true,
+	})
+	evCharge.associate = function (models) {
+		//gasStation.hasMany(models.rating, {foreignKey: 'site_uid'})
+		evCharge.hasMany(models.rating, {
+			foreignKey: 'targetUid',
+			constraints: false,
+			scope: {
+				targetType: 1
+			}
+		})
+		evCharge.hasMany(models.favorite, {
+			foreignKey: 'targetUid',
+			constraints: false,
+			scope: {
+				targetType: 1
+			}
+		})
+	}
+	evCharge.getByUid = async function (ctx, uid) {
+		let data = await evCharge.findByPk(uid)
+		if (!data) {
+			response.badRequest(ctx)
+		}
+		return data
+	}
+
+	evCharge.search = async (params, models) => {
+		let where = {}
+		let order = [['createdAt', 'DESC']]
+
+		let result = await evCharge.findAll({
+			order: order,
+			where: where
+		})
+		return result
+	}
+
+	evCharge.userSearch = async (params, models) => {
+		let where = {}
+		let order = [['createdAt', 'DESC']]
+
+		let result = await evCharge.findAll({
+			order: order,
+			where: [
+				where
+			]
+		})
+		return result
+	}
+
+	return evCharge
+}
