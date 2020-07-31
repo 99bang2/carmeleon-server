5433fafa6f2af0a0eb73cfeadc3d38e9ddd5fbcd
Jonghyun <jonghyun7147@gmail.com>
2020-07-31 11:51:55 +0900

    discount Migration

---
+++ sequelize-migration/migrations/20200731075812-discountTicket.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+	  try {
+		  await queryInterface.addColumn('discount_tickets', 'ticket_price_discount', {
+			  type: Sequelize.INTEGER
+		  });
+		  await queryInterface.addColumn('discount_tickets', 'ticket_price_discount_percent', {
+			  type: Sequelize.INTEGER
+		  });
+		  return Promise.resolve();
+	  } catch (e) {
+		  return Promise.reject(e);
+	  }
+	  // return [
+		//   queryInterface.addColumn('discount_tickets', 'ticket_price_discount', {
+		// 	  type: Sequelize.INTEGER
+		//   }),
+		//   queryInterface.addColumn('discount_tickets', 'ticket_price_discount_percent', {
+		// 	  type: Sequelize.INTEGER,
+		//   })
+	  // ];
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+	  // return [
+		//   queryInterface.removeColumn('discount_tickets', 'ticket_price_discount'),
+		//   queryInterface.removeColumn('discount_tickets', 'ticket_price_discount_percent')
+	  // ];
+	  try {
+		  await queryInterface.removeColumn('discount_tickets', 'ticket_price_discount');
+		  await queryInterface.removeColumn('discount_tickets', 'ticket_price_discount_percent');
+		  return Promise.resolve();
+	  } catch (e) {
+		  return Promise.reject(e);
+	  }
+  }
+};
