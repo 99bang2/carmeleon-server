387d1b4cf499fd2cdc39e79873c1eb960172a8d8
Jonghyun <jonghyun7147@gmail.com>
2020-08-18 16:02:35 +0900

    favorite update

--- models/favorite.js
+++ models/favorite.js
@@ -45,6 +45,11 @@ module.exports = (sequelize, DataTypes) => {
 			constraints: false,
 			as: 'carWash'
 		})
+		favorite.belongsTo(models.evCharge, {
+			foreignKey: 'targetUid',
+			constraints: false,
+			as: 'evCharge'
+		})
 	}
 	favorite.getByUid = async function (ctx, uid) {
 		let data = await favorite.findByPk(uid)
@@ -57,16 +62,89 @@ module.exports = (sequelize, DataTypes) => {
 		let data = await favorite.findAll({
 			include: [{
 				as: 'parkingSite',
-				model: models.parkingSite
+				model: models.parkingSite,
+				attributes: ['uid', 'name', 'address']
 			},{
 				as: 'gasStation',
-				model: models.gasStation
+				model: models.gasStation,
+				attributes: ['uid', 'gasStationName', 'address']
 			},{
 				as: 'carWash',
-				model: models.carWash
-			},],
+				model: models.carWash,
+				attributes: ['uid', 'carWashName', 'address']
+			},{
+				as: 'evCharge',
+				model: models.evCharge,
+				attributes: ['uid', 'statNm', 'addr']
+			},
+			],
 			where: {userUid:userUid}
-		})
+		}).then(function(val){
+			val.map(function (obj) {
+				console.log(obj.dataValues)
+				let tempVal = {}
+				switch (obj.dataValues.targetType) {
+					case 0:
+						if(!obj.dataValues.parkingSite){
+							ctx.throw({
+								code: 400,
+								message: '존재 하지 않습니다.'
+							})
+						}
+						tempVal.uid = obj.dataValues.parkingSite.uid
+						tempVal.name = obj.dataValues.parkingSite.name
+						tempVal.address = obj.dataValues.parkingSite.address
+						break;
+					case 1:
+						if(!obj.dataValues.gasStation){
+							ctx.throw({
+								code: 400,
+								message: '존재 하지 않습니다.'
+							})
+						}
+						tempVal.uid = obj.dataValues.gasStation.uid
+						tempVal.name = obj.dataValues.gasStation.gasStationName
+						tempVal.address = obj.dataValues.gasStation.address
+						break;
+					case 2:
+						if(!obj.dataValues.carWash){
+							ctx.throw({
+								code: 400,
+								message: '존재 하지 않습니다.'
+							})
+						}
+						tempVal.uid = obj.dataValues.carWash.uid
+						tempVal.name = obj.dataValues.carWash.carWashName
+						tempVal.address = obj.dataValues.carWash.address
+						break;
+					case 3:
+						if(!obj.dataValues.evCharge){
+							ctx.throw({
+								code: 400,
+								message: '존재 하지 않습니다.'
+							})
+						}
+						tempVal.uid = obj.dataValues.evCharge.uid
+						tempVal.name = obj.dataValues.evCharge.statNm
+						tempVal.address = obj.dataValues.evCharge.addr
+						break;
+				}
+				obj.dataValues.place = tempVal
+				if(obj.dataValues.parkingSite){
+					delete obj.dataValues.parkingSite
+				}
+				if(obj.dataValues.gasStation){
+					delete obj.dataValues.gasStation
+				}
+				if(obj.dataValues.carWash) {
+					delete obj.dataValues.carWash
+				}
+				if(obj.dataValues.evCharge) {
+					delete obj.dataValues.evCharge
+				}
+			})
+			return val
+		}).catch()
 		if (!data) {
 			response.badRequest(ctx)
 		}
