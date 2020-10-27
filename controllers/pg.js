c6d9a0743e864a266f73bb90eb326b611b00ede6
JaegyuJeong <h923313@gmail.com>
2020-10-27 15:09:28 +0900

    pg process update

--- controllers/pg.js
+++ controllers/pg.js
@@ -6,88 +6,102 @@ const axios = require('axios')
 const moment = require('moment')
 
 exports.pgSave = async function (ctx) {
-	let _ = ctx.request.body
-	let flg = _.resultcode === '00'
-
-	if (flg) {
-		let insertData = {}
-		let data = {}
-		data.cardCode = _.cardcd
-		data.cardNumber = _.cardno
-		data.billKey = _.billkey
-		insertData.userUid = _.p_noti
-		insertData.cardInfo = data
-		await models.card.create(insertData)
-	}
-
-	ctx.redirect(`http://192.168.0.101:3000/redirect?success=${flg}`)
+    let _ = ctx.request.body
+    let flg = _.resultcode === '00'
+    if (flg) {
+        let insertData = {}
+        let data = {}
+        data.cardCode = _.cardcd
+        data.cardNumber = _.cardno
+        data.billKey = _.billkey
+        insertData.userUid = _.p_noti
+        insertData.cardInfo = data
+        await models.card.create(insertData)
+    }
+    ctx.redirect(`http://192.168.0.101:3000/redirect?success=${flg}`)
 }
 exports.pgPayment = async function (ctx) {
-	let _ = ctx.request.body
-	let serverName = os.hostname()
-	let clientIP = ctx.ip
-	let dataArr = []
-	let beforeHash = "rKnPljRn5m6J9MzzBillingCard" + moment().format("YYYYMMDDHHiiss") +
-		_.clientIp + _.mid + _.orderId + _.price + _.billKey
-	dataArr["type"] = "Billing"
-	dataArr["paymethod"] = "Card"
-	dataArr["timestamp"] = moment().format("YYYYMMDDHHiiss")
-	dataArr["clientIp"] = _.clientIp
-	dataArr["mid"] = _.mid
-	dataArr["url"] = serverName
-	dataArr["moid"] = _.orderId
-	dataArr["goodName"] = _.goodName
-	dataArr["buyerName"] = _.buyerName
-	dataArr["buyerEmail"] = _.buyerEmail
-	dataArr["buyerTel"] = _.buyerTel
-	dataArr["price"] = _.price
-	dataArr["billKey"] = _.billKey
-	dataArr["authentification"] = "00"
-	dataArr["hashData"] = SHA512(beforeHash).toString()
-	let queryString = generateQueryString(dataArr)
-	let res = await axios.post('https://iniapi.inicis.com/api/v1/billing?' + encodeURI(queryString))
-	if (res.data.resultCode === '00') {
-		response.send(ctx, {
-			result: true,
-			msg: res.data.resultMsg,
-			tid: res.data.tid
-		})
-	} else {
-		response.send(ctx, {
-			result: false,
-			msg: res.data.resultMsg
-		})
-	}
+    let _ = ctx.request.body
+    let payLogUid = _.payLogUid
+    let serverName = os.hostname()
+    let clientIP = ctx.ip
+    let dataArr = []
+    let beforeHash = "rKnPljRn5m6J9MzzBillingCard" + moment().format("YYYYMMDDHHiiss") +
+        _.clientIp + _.mid + _.orderId + _.price + _.billKey
+    dataArr["type"] = "Billing"
+    dataArr["paymethod"] = "Card"
+    dataArr["timestamp"] = moment().format("YYYYMMDDHHiiss")
+    dataArr["clientIp"] = _.clientIp
+    dataArr["mid"] = _.mid
+    dataArr["url"] = serverName
+    dataArr["moid"] = _.orderId
+    dataArr["goodName"] = _.goodName
+    dataArr["buyerName"] = _.buyerName
+    dataArr["buyerEmail"] = _.buyerEmail
+    dataArr["buyerTel"] = _.buyerTel
+    dataArr["price"] = _.price
+    dataArr["billKey"] = _.billKey
+    dataArr["authentification"] = "00"
+    dataArr["hashData"] = SHA512(beforeHash).toString()
+    let queryString = generateQueryString(dataArr)
+    let res = await axios.post('https://iniapi.inicis.com/api/v1/billing?' + encodeURI(queryString))
+    let payInfo = {
+        orderId: _.orderId,
+        clientIP: _.clientIp,
+        tid: res.data.tid
+    }
+    if (res.data.resultCode === '00') {
+        models.payLog.update(
+            {
+                status: 10,
+                payInfo: payInfo
+            }, {where: {uid: payLogUid}})
+        response.send(ctx, {
+            result: true,
+            msg: res.data.resultMsg
+        })
+    } else {
+        models.payLog.update(
+            {
+                status: -10,
+                payInfo: payInfo
+            }, {where: {uid: payLogUid}})
+        response.send(ctx, {
+            result: false,
+            msg: res.data.resultMsg
+        })
+    }
 }
-exports.pgCancel = async function (ctx){
-	let {uid} = ctx.params
-	let payInfo = await models.payLog.getByUid(ctx, uid, models)
-	let dataArr = []
-	let mid = 'INIBillTst'
-	let beforeHash = "rKnPljRn5m6J9MzzRefundCard" + moment().format("YYYYMMDDHHiiss") + payInfo.payInfo.clientIp + mid + payInfo.payInfo.tid
-	dataArr["type"] = "Refund"
-	dataArr["paymethod"] = "Card"
-	dataArr["timestamp"] = moment().format("YYYYMMDDHHiiss")
-	dataArr["clientIp"] = payInfo.payInfo.clientIp
-	dataArr["mid"] = mid
-	dataArr["tid"] = payInfo.payInfo.tid
-	dataArr["msg"] = "관리자 취소" // 취소 사유
-	dataArr["hashData"] = SHA512(beforeHash).toString()
-	let queryString = generateQueryString(dataArr)
-	let res = await axios.post('https://iniapi.inicis.com/api/v1/refund?' + encodeURI(queryString))
-	if (res.data.resultCode === '00') {
-		response.send(ctx, {
-			result: true
-		})
-	} else {
-		response.send(ctx, {
-			result: false,
-			msg: res.data.resultMsg
-		})
-	}
+exports.pgCancel = async function (ctx) {
+    let {uid} = ctx.params
+    let payInfo = await models.payLog.getByUid(ctx, uid, models)
+    let dataArr = []
+    let mid = 'INIBillTst'
+    let beforeHash = "rKnPljRn5m6J9MzzRefundCard" + moment().format("YYYYMMDDHHiiss") + payInfo.payInfo.clientIp + mid + payInfo.payInfo.tid
+    dataArr["type"] = "Refund"
+    dataArr["paymethod"] = "Card"
+    dataArr["timestamp"] = moment().format("YYYYMMDDHHiiss")
+    dataArr["clientIp"] = payInfo.payInfo.clientIp
+    dataArr["mid"] = mid
+    dataArr["tid"] = payInfo.payInfo.tid
+    dataArr["msg"] = "관리자 취소" // 취소 사유
+    dataArr["hashData"] = SHA512(beforeHash).toString()
+    let queryString = generateQueryString(dataArr)
+    let res = await axios.post('https://iniapi.inicis.com/api/v1/refund?' + encodeURI(queryString))
+    if (res.data.resultCode === '00') {
+        payInfo.update({status: -20})
+        response.send(ctx, {
+            result: true
+        })
+    } else {
+        response.send(ctx, {
+            result: false,
+            msg: res.data.resultMsg
+        })
+    }
 
 }
 
 function generateQueryString(object) {
-	return Object.keys(object).map(key => `${key}=${object[key]}`).join('&')
+    return Object.keys(object).map(key => `${key}=${object[key]}`).join('&')
 }
--- models/payLog.js
+++ models/payLog.js
@@ -5,218 +5,219 @@ const moment = require('moment')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 module.exports = (sequelize, DataTypes) => {
-	const payLog = sequelize.define('payLog', {
-		uid: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			autoIncrement: true,
-			primaryKey: true,
-		},
-		//// 사용 내역 정보 /////
-		payInfo: {
-			type: DataTypes.JSON
-		},
-		// or carModel uid //
-		carNumber: {
-			type: DataTypes.STRING
-		},
-		phoneNumber: {
-			type: DataTypes.STRING
-		},
-		reserveTime: {
-			type: DataTypes.STRING
-		},
-		payType: {
-			type: DataTypes.STRING
-		},
-		payTypeName: {
-			type: DataTypes.VIRTUAL,
-			get: function () {
-				if (this.getDataValue('payType') !== null) {
-					return codes.paymentTag[this.getDataValue('payType')]
-				}
-			}
-		},
-		status: {
-			type: DataTypes.INTEGER
-		},
-		price: {
-			type: DataTypes.INTEGER
-		},
-		discountPrice: {
-			type: DataTypes.INTEGER
-		},
-		totalPrice: {
-			type: DataTypes.INTEGER
-		},
-		fee: {
-			type: DataTypes.INTEGER
-		},
-		visible: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: true
-		},
-		///////////////////////
-		userUid: {
-			type: DataTypes.INTEGER
-		},
-		siteUid: {
-			type: DataTypes.INTEGER
-		},
-		discountTicketUid: {
-			type: DataTypes.INTEGER
-		},
-		cardUid: {
-			type: DataTypes.INTEGER
-		},
-		rateUid: {
-			type: DataTypes.INTEGER
-		}
-	}, {
-		timestamps: true,
-		underscored: true,
-		paranoid: true
-	})
-	payLog.associate = function (models) {
-		payLog.belongsTo(models.user)
-		payLog.belongsTo(models.parkingSite, {foreignKey: 'site_uid', targetKey: 'uid'})
-		payLog.belongsTo(models.discountTicket)
-		payLog.belongsTo(models.card)
-	}
-	payLog.getByUid = async function (ctx, uid, models) {
-		let data = await payLog.findByPk(uid, {
-			include: [
-				{
-					model: models.parkingSite,
-				}, {
-					model: models.discountTicket,
-				}, {
-					model: models.card,
-				}, {
-					model: models.user,
-				},
-			]
-		})
-		if (!data) {
-			response.badRequest(ctx)
-		}
-		return data
-	}
-	payLog.search = async (params, models) => {
-		let where = {}
-		let offset = null
-		let limit = null
-		let order = [['createdAt', 'DESC']]
-		if (params.searchData) {
-			let searchData = JSON.parse(params.searchData)
-			if (searchData.searchKeyword) {
-				where = {
-					[Op.or]: [
-						{
-							carNumber: {
-								[Op.like]: '%' + searchData.searchKeyword + '%'
-							}
-						},
-						{
-							phoneNumber: {
-								[Op.like]: '%' + searchData.searchKeyword + '%'
-							}
-						},
-						{
-							price: {
-								[Op.like]: '%' + searchData.searchKeyword + '%'
-							}
-						},
-						{
-							discountPrice: {
-								[Op.like]: '%' + searchData.searchKeyword + '%'
-							}
-						}
-					]
-				}
-			}
+    const payLog = sequelize.define('payLog', {
+        uid: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        //// 사용 내역 정보 /////
+        payInfo: {
+            type: DataTypes.JSON
+        },
+        // or carModel uid //
+        carNumber: {
+            type: DataTypes.STRING
+        },
+        phoneNumber: {
+            type: DataTypes.STRING
+        },
+        reserveTime: {
+            type: DataTypes.STRING
+        },
+        payType: {
+            type: DataTypes.STRING
+        },
+        payTypeName: {
+            type: DataTypes.VIRTUAL,
+            get: function () {
+                if (this.getDataValue('payType') !== null) {
+                    return codes.paymentTag[this.getDataValue('payType')]
+                }
+            }
+        },
+        status: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        price: {
+            type: DataTypes.INTEGER
+        },
+        discountPrice: {
+            type: DataTypes.INTEGER
+        },
+        totalPrice: {
+            type: DataTypes.INTEGER
+        },
+        fee: {
+            type: DataTypes.INTEGER
+        },
+        visible: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        ///////////////////////
+        userUid: {
+            type: DataTypes.INTEGER
+        },
+        siteUid: {
+            type: DataTypes.INTEGER
+        },
+        discountTicketUid: {
+            type: DataTypes.INTEGER
+        },
+        cardUid: {
+            type: DataTypes.INTEGER
+        },
+        rateUid: {
+            type: DataTypes.INTEGER
+        }
+    }, {
+        timestamps: true,
+        underscored: true,
+        paranoid: true
+    })
+    payLog.associate = function (models) {
+        payLog.belongsTo(models.user)
+        payLog.belongsTo(models.parkingSite, {foreignKey: 'site_uid', targetKey: 'uid'})
+        payLog.belongsTo(models.discountTicket)
+        payLog.belongsTo(models.card)
+    }
+    payLog.getByUid = async function (ctx, uid, models) {
+        let data = await payLog.findByPk(uid, {
+            include: [
+                {
+                    model: models.parkingSite,
+                }, {
+                    model: models.discountTicket,
+                }, {
+                    model: models.card,
+                }, {
+                    model: models.user,
+                },
+            ]
+        })
+        if (!data) {
+            response.badRequest(ctx)
+        }
+        return data
+    }
+    payLog.search = async (params, models) => {
+        let where = {}
+        let offset = null
+        let limit = null
+        let order = [['createdAt', 'DESC']]
+        if (params.searchData) {
+            let searchData = JSON.parse(params.searchData)
+            if (searchData.searchKeyword) {
+                where = {
+                    [Op.or]: [
+                        {
+                            carNumber: {
+                                [Op.like]: '%' + searchData.searchKeyword + '%'
+                            }
+                        },
+                        {
+                            phoneNumber: {
+                                [Op.like]: '%' + searchData.searchKeyword + '%'
+                            }
+                        },
+                        {
+                            price: {
+                                [Op.like]: '%' + searchData.searchKeyword + '%'
+                            }
+                        },
+                        {
+                            discountPrice: {
+                                [Op.like]: '%' + searchData.searchKeyword + '%'
+                            }
+                        }
+                    ]
+                }
+            }
 
-			if (searchData.searchDate) {
-				if (searchData.searchDate.split('~').length > 1) {
-					where.createdAt = {
-						[Op.between]: [
-							moment(searchData.searchDate.split(' ~ ')[0]).format('YYYY-MM-DD'),
-							moment(searchData.searchDate.split(' ~ ')[1]).add(1, 'days').format('YYYY-MM-DD')
-						]
-					}
-				} else {
-					where.createdAt = {
-						[Op.between]: [
-							moment(searchData.searchDate).format('YYYY-MM-DD'),
-							moment(searchData.searchDate).add(1, 'days').format('YYYY-MM-DD')
-						]
-					}
-				}
-			}
-		}
-		if (params.userUid) {
-			where.userUid = params.userUid
-		}
-		if (params.visible) {
-			where.visible = params.visible
-		}
-		if (params.carNumber) {
-			where.carNumber = {[Op.substring]: params.carNumber}
-		}
-		if (params.page) {
-			//offset, limit 처리//
-			limit = 10
-			offset = (Number(params.page) - 1) * limit
-		}
-		let rateWhere = 'target_type = 0 AND target_uid = payLog.site_uid AND user_uid = payLog.user_uid)'
-		let result = await payLog.findAll({
-			//TODO:추후 필요한 사항만 attribute 넣어 놓을 것
-			attributes: {
-				include: [
-					[`(SELECT count(uid) FROM ratings WHERE ` + rateWhere, 'rate_count']
-				]
-			},
-			include: [
-				{
-					model: models.parkingSite,
-				}, {
-					model: models.discountTicket,
-				}, {
-					model: models.user,
-				},
-			],
-			offset: offset,
-			limit: limit,
-			where: where,
-			order: order
-		})
-		let count = await payLog.count({
-			where: where
-		})
-		return {
-			rows: result,
-			count: count
-		}
-	}
-	payLog.getByUserUid = async function (ctx, uid, models) {
-		let data = await payLog.findAll({
-			include: [
-				{
-					model: models.parkingSite,
-				}, {
-					model: models.discountTicket,
-				}
-			],
-			where: {
-				userUid: uid,
-				visible: true
-			},
-			order: [['createdAt', 'DESC']]
-		})
-		if (!data) {
-			response.badRequest(ctx)
-		}
-		return data
-	}
-	return payLog
+            if (searchData.searchDate) {
+                if (searchData.searchDate.split('~').length > 1) {
+                    where.createdAt = {
+                        [Op.between]: [
+                            moment(searchData.searchDate.split(' ~ ')[0]).format('YYYY-MM-DD'),
+                            moment(searchData.searchDate.split(' ~ ')[1]).add(1, 'days').format('YYYY-MM-DD')
+                        ]
+                    }
+                } else {
+                    where.createdAt = {
+                        [Op.between]: [
+                            moment(searchData.searchDate).format('YYYY-MM-DD'),
+                            moment(searchData.searchDate).add(1, 'days').format('YYYY-MM-DD')
+                        ]
+                    }
+                }
+            }
+        }
+        if (params.userUid) {
+            where.userUid = params.userUid
+        }
+        if (params.visible) {
+            where.visible = params.visible
+        }
+        if (params.carNumber) {
+            where.carNumber = {[Op.substring]: params.carNumber}
+        }
+        if (params.page) {
+            //offset, limit 처리//
+            limit = 10
+            offset = (Number(params.page) - 1) * limit
+        }
+        let rateWhere = 'target_type = 0 AND target_uid = payLog.site_uid AND user_uid = payLog.user_uid)'
+        let result = await payLog.findAll({
+            //TODO:추후 필요한 사항만 attribute 넣어 놓을 것
+            attributes: {
+                include: [
+                    [`(SELECT count(uid) FROM ratings WHERE ` + rateWhere, 'rate_count']
+                ]
+            },
+            include: [
+                {
+                    model: models.parkingSite,
+                }, {
+                    model: models.discountTicket,
+                }, {
+                    model: models.user,
+                },
+            ],
+            offset: offset,
+            limit: limit,
+            where: where,
+            order: order
+        })
+        let count = await payLog.count({
+            where: where
+        })
+        return {
+            rows: result,
+            count: count
+        }
+    }
+    payLog.getByUserUid = async function (ctx, uid, models) {
+        let data = await payLog.findAll({
+            include: [
+                {
+                    model: models.parkingSite,
+                }, {
+                    model: models.discountTicket,
+                }
+            ],
+            where: {
+                userUid: uid,
+                visible: true
+            },
+            order: [['createdAt', 'DESC']]
+        })
+        if (!data) {
+            response.badRequest(ctx)
+        }
+        return data
+    }
+    return payLog
 }
