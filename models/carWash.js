91dd6e57d9dd382c0271e60b48010c4fea3cbd41
JaegyuJeong <h923313@gmail.com>
2020-11-03 11:13:11 +0900

    add bookingCode in model carWash

--- models/carWash.js
+++ models/carWash.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
 			autoIncrement: true,
 			primaryKey: true,
 		},
+		bookingCode:{
+			type:DataTypes.STRING
+		},
 		carWashName: {
 			type: DataTypes.STRING,
 			allowNull: false
