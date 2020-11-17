b9fabcd1d10146e78c66d8c43a47273030a65ecf
Jonghyun <jonghyun7147@gmail.com>
2020-11-17 13:56:18 +0900

    rollback api

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -3,15 +3,16 @@ const Router = require('koa-router')
 const api = new Router()
 const adminRouter = require('../routes/admin')
 const userRouter = require('../routes/user')
+const auth = require('../libs/auth')
 /*
 * Carmelon Sever
 * */
 /**
  * admin&user Route
  */
-api.use('/admin', auth.isAdminLoggedIn, adminRouter.routes())
-api.use('', auth.isUserLoggedIn, userRouter.routes())
-// api.use('/admin',  adminRouter.routes())
-// api.use('', userRouter.routes())
+// api.use('/admin', auth.isAdminLoggedIn, adminRouter.routes())
+// api.use('', auth.isUserLoggedIn, userRouter.routes())
+api.use('/admin',  adminRouter.routes())
+api.use('', userRouter.routes())
 
 module.exports = api
