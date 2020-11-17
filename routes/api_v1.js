573e6b6c2bfa1cca84e1b031937065f527384dc3
Jonghyun <jonghyun7147@gmail.com>
2020-11-17 15:49:44 +0900

    route update (user)

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -10,7 +10,7 @@ const auth = require('../libs/auth')
 /**
  * admin&user Route
  */
-// api.use('/admin', auth.isAdminLoggedIn, adminRouter.routes())
+api.use('/admin', auth.isAdminLoggedIn, adminRouter.routes())
 // api.use('', auth.isUserLoggedIn, userRouter.routes())
 api.use('/admin',  adminRouter.routes())
 api.use('', userRouter.routes())
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@
 const Router = require('koa-router')
 const api = new Router()
 
+const auth = require('../libs/auth')
 const userController = require('../controllers/user')
 const noticeController = require('../controllers/notice')
 const eventController = require('../controllers/event')
@@ -32,7 +33,7 @@ const commonController = require('../controllers/common')
  */
 
 api.post('/users/login', userController.login)
-api.get('/users/logout', userController.logout)
+api.get('/users/logout', auth.isUserLoggedIn, userController.logout)
 api.get('/users/check', userController.check)
 
 /**
@@ -68,72 +69,68 @@ api.get('/carWashes/:uid', carWashController.read)
 /**
  * 리뷰 관리
  */
-api.post('/rates/:targetType/:targetUid', commonController.isAvailableTarget, rateController.create)
-api.get('/rates/:targetType/:targetUid', commonController.isAvailableTarget, rateController.targetList)
+api.post('/rates/:targetType/:targetUid', [auth.isUserLoggedIn, commonController.isAvailableTarget], rateController.create)
+api.get('/rates/:targetType/:targetUid', rateController.targetList)
 api.get('/rates', rateController.list)
 api.get('/rates/:uid', rateController.userList)
-api.put('/rates/:uid', rateController.update)
-api.delete('/rates/:uid', rateController.delete)
-api.get('/rateChecks' , rateController.checkAvailable)
+api.put('/rates/:uid', auth.isUserLoggedIn, rateController.update)
+api.delete('/rates/:uid', auth.isUserLoggedIn, rateController.delete)
+api.get('/rateChecks', auth.isUserLoggedIn, rateController.checkAvailable)
 /* 리뷰 꿀팁 */
-api.post('/rateTips', rateTipController.create)
+api.post('/rateTips', auth.isUserLoggedIn, rateTipController.create)
 /**
  * 즐겨찾기 관리
  */
-api.post('/favorites', favoriteController.create)
-api.get('/favorites/:userUid', favoriteController.userList)
+api.post('/favorites', auth.isUserLoggedIn, favoriteController.create)
+api.get('/favorites/:userUid', auth.isUserLoggedIn, favoriteController.userList)
 /**
  * 유저 관련 컨트롤러
  */
 api.post('/users', userController.create)
-api.get('/users/:uid', userController.read)
-api.put('/users/:uid', userController.update)
-api.delete('/users/:uid', userController.delete)
-api.get('/getBadge/:uid', userController.getBadge)
+api.get('/users/:uid', auth.isUserLoggedIn, userController.read)
+api.put('/users/:uid', auth.isUserLoggedIn, userController.update)
+api.get('/getBadge/:uid', auth.isUserLoggedIn, userController.getBadge)
 
-api.post('/cars', carController.create)
-api.get('/cars', carController.list)
-api.get('/cars/:uid', carController.read)
-api.put('/cars/:uid', carController.update)
-api.delete('/cars/:uid', carController.delete)
-api.put('/mainCars', carController.isMain)
-api.get('/carLists/:userUid', carController.carList)
+api.post('/cars', auth.isUserLoggedIn, carController.create)
+api.get('/cars', auth.isUserLoggedIn, carController.list)
+api.get('/cars/:uid', auth.isUserLoggedIn, carController.read)
+api.put('/cars/:uid', auth.isUserLoggedIn, carController.update)
+api.delete('/cars/:uid', auth.isUserLoggedIn, carController.delete)
+api.put('/mainCars', auth.isUserLoggedIn, carController.isMain)
+api.get('/carLists/:userUid', auth.isUserLoggedIn, carController.carList)
 
-api.post('/cards', cardController.create)
-api.get('/cards', cardController.list)
-api.get('/cards/:uid', cardController.read)
-api.put('/cards/:uid', cardController.update)
-api.delete('/cards/:uid', cardController.delete)
-api.put('/mainCards', cardController.isMain)
-api.get('/cardLists/:userUid', cardController.cardList)
+api.post('/cards', auth.isUserLoggedIn, cardController.create)
+api.get('/cards', auth.isUserLoggedIn, cardController.list)
+api.get('/cards/:uid', auth.isUserLoggedIn, cardController.read)
+api.put('/cards/:uid', auth.isUserLoggedIn, cardController.update)
+api.delete('/cards/:uid', auth.isUserLoggedIn, cardController.delete)
+api.put('/mainCards', auth.isUserLoggedIn, cardController.isMain)
+api.get('/cardLists/:userUid', auth.isUserLoggedIn, cardController.cardList)
 
 api.get('/discountTickets', discountTicketController.list)
 api.get('/discountTickets/:uid', discountTicketController.read)
 
-api.get('/points', pointLogController.list)
-api.get('/points/:uid', pointLogController.read)
-api.get('/pointLogs/:userUid', pointLogController.userList)
+api.get('/points', auth.isUserLoggedIn, pointLogController.list)
+api.get('/points/:uid', auth.isUserLoggedIn, pointLogController.read)
+api.get('/pointLogs/:userUid', auth.isUserLoggedIn, pointLogController.userList)
 
-api.post('/payLogs', payLogController.create)
-api.get('/payLogs', payLogController.list)
-api.get('/payLogs/:uid', payLogController.read)
-api.put('/payLogs/:uid', payLogController.update)
+api.post('/payLogs', auth.isUserLoggedIn, payLogController.create)
+api.get('/payLogs', auth.isUserLoggedIn, payLogController.list)
+api.get('/payLogs/:uid', auth.isUserLoggedIn, payLogController.read)
+api.put('/payLogs/:uid', auth.isUserLoggedIn, payLogController.update)
 
-api.get('/coupons', couponController.list)
-api.get('/coupons/:uid', couponController.read)
+api.get('/coupons', auth.isUserLoggedIn, couponController.list)
+api.get('/coupons/:uid', auth.isUserLoggedIn, couponController.read)
 
-api.get('/couponLogs', couponLogController.list)
-api.get('/couponLogs/:uid', couponLogController.read)
+api.get('/couponLogs', auth.isUserLoggedIn, couponLogController.list)
+api.get('/couponLogs/:uid', auth.isUserLoggedIn, couponLogController.read)
 
-api.get('/reviewTemplates', reviewTemplateController.list)
-api.get('/reviewTemplates/:uid', reviewTemplateController.read)
-
-api.post('/questions', questionController.create)
+api.post('/questions', auth.isUserLoggedIn, questionController.create)
 //필요 없을 시 삭제//
-api.get('/questions', questionController.list)
-api.get('/questions/:uid', questionController.read)
+api.get('/questions', auth.isUserLoggedIn, questionController.list)
+api.get('/questions/:uid', auth.isUserLoggedIn, questionController.read)
 //알림 리스트//
- api.get('/pushes', pushController.userList)
+ api.get('/pushes', auth.isUserLoggedIn, pushController.userList)
 ///////////////////
 /**
  * 테슬라 컨트롤러
@@ -151,8 +148,8 @@ api.post('/searchList', commonController.searchList)
 api.post('/avgRate', commonController.avgRate)
 api.post('/codes', commonController.codes)
 
-api.post('/pgBillNice', pgController.pgBillNice)
-api.post('/pgPaymentNice', pgController.pgPaymentNice)
-api.post('/pgPaymentCancelNice', pgController.pgPaymentCancelNice)
+api.post('/pgBillNice', auth.isUserLoggedIn, pgController.pgBillNice)
+api.post('/pgPaymentNice', auth.isUserLoggedIn, pgController.pgPaymentNice)
+api.post('/pgPaymentCancelNice', auth.isUserLoggedIn, pgController.pgPaymentCancelNice)
 
 module.exports = api
