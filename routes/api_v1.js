fff51df47c4881dad3ea9aa6962a421cd70f40aa
Jonghyun <jonghyun7147@gmail.com>
2020-11-17 11:31:26 +0900

    route add login check

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -9,9 +9,9 @@ const userRouter = require('../routes/user')
 /**
  * admin&user Route
  */
-// api.use('/admin', auth.isAdminLoggedIn, adminRouter.routes())
-// api.use('', auth.isUserLoggedIn, userRouter.routes())
-api.use('/admin',  adminRouter.routes())
-api.use('', userRouter.routes())
+api.use('/admin', auth.isAdminLoggedIn, adminRouter.routes())
+api.use('', auth.isUserLoggedIn, userRouter.routes())
+// api.use('/admin',  adminRouter.routes())
+// api.use('', userRouter.routes())
 
 module.exports = api
