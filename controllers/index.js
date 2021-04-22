ca1947fd2876346198747ee2d88d542c979cda4d
chominsu <agilityaaa@gmail.com>
2021-04-22 16:21:11 +0900

    ticket active api 수정

--- controllers/index.js
+++ controllers/index.js
@@ -11,8 +11,16 @@ exports.ticketActive = async function (ctx) {
     }
 
     let parkingSite = await models.parkingSite.findByPk(macroId)
-    parkingSite.isBuy = active
-    await parkingSite.save()
 
-    response.send(ctx, true)
+    if (parkingSite.siteType !== 0) {
+        response.send(ctx, {
+            data: false,
+            message: "하이파킹 주차장이 아닙니다."
+        })
+    } else {
+        parkingSite.isBuy = active
+        await parkingSite.save()
+
+        response.send(ctx, true)
+    }
 }
