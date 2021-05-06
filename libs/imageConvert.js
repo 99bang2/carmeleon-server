3f30c784ad43edcec684989af4cc9c8050ccddb5
JaegyuJeong <h923313@gmail.com>
2021-05-06 18:01:06 +0900

    jpeg auto rotate

--- libs/imageConvert.js
+++ libs/imageConvert.js
@@ -5,6 +5,8 @@ const sharp         = require('./sharp')
 const jimp          = require('./jimp')
 const fs            = require('fs')
 const config        = require('../configs/objectStorage.json')
+const jo = require("jpeg-autorotate");
+const Jimp = require("jimp");
 
 function getName(url) {
     let split = url.split('/')
@@ -40,6 +42,21 @@ module.exports = async (images, prefix) => {
             let watermarkedImage = await jimp.addWaterMark(url)
 
             await watermarkedImage.writeAsync(local_watermark_path)
+
+            /////////////////////////// jpeg-autorotate///////////////////////////////
+            await jo.rotate(local_watermark_path)
+                .then(async ({orientation}) => {
+                    if(orientation !== null && orientation === 6) {
+                        let image = await Jimp.read(local_watermark_path)
+                        await image.rotate(180).writeAsync(local_watermark_path)
+                    }
+            }).catch((error) => {
+                if(error.code === jo.errors.correct_orientation) {
+                    console.log('The orientation of this image is already correct!')
+                }
+            })
+            //////////////////////////////////////////////////////////////////////////
+
             await objectStorage.uploadStream(fs.createReadStream(local_watermark_path), name, prefix)
 
             let image = config.url + '/' + config.bucket_name + '/watermark/' + prefix + '/' + name
