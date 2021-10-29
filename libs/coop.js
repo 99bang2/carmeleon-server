6bfb07f30ae495113afef581b9845bed247875d0
mschoi <mschoi@mobilx.co.kr>
2021-10-29 17:10:15 +0900

    coop content-length

--- libs/coop.js
+++ libs/coop.js
@@ -13,19 +13,19 @@ const consola = require('consola')
 
 const header = {
     'Content-Type': 'text/xml;charset=UTF-8',
-    'Content-Length': 'length',
+    //'Content-Length': 'length',
     SOAPAction: null,
 }
 
 module.exports = {
     async useCoupon(info) {
         header.SOAPAction = api.action + '/Use'
-        
+
         let url = api.url + '?op=Use'
         let day = moment().format('YYYYMMDD')
         let time = moment().format('HHmmss')
         let productNumber = day + time
-        
+
         let data = {
             couponNumber: encrypt(info.couponNumber),
             price: info.price,
@@ -34,7 +34,7 @@ module.exports = {
 
         let xml = requestXMLForm('use', data)
         consola.info('Send XML Data: (Use)', { xml })
-        
+
         try {
             let res = await soap({
                 method: 'POST',
@@ -43,17 +43,17 @@ module.exports = {
                 xml: xml,
                 timeout: 5000,
             })
-            
+
             let parser = new xml2js.Parser()
-            
+
             return new Promise((resolve, reject) => {
-                
+
                 parser.parseString(res.response.body, (err, result) => {
                     if (err) {
                         consola.error('XML Parsing Error', err)
                         reject(err)
                     }
-                    
+
                     let envelope = Object.values(result)
                     let body = Object.values(envelope[0])
                     let useInfo = body[1][0].UseResponse
@@ -64,7 +64,7 @@ module.exports = {
                         resolve('SERVERERROR')
                     }
                     let aprvResult = useResult[0].AprvResult[0]
-                    
+
                     if (apiResult.$.ResultCd !== '0000') {
                         consola.error(`COOP ERROR: CODE ${apiResult.$.ResultCd}`)
                         resolve('MISMATCH')
@@ -73,7 +73,7 @@ module.exports = {
                         consola.error('COOP ERROR: Coupon Inactive')
                         resolve('INACTIVE')
                     }
-                    
+
                     resolve({
                         couponNumber: info.couponNumber,
                         price: info.price,
@@ -94,15 +94,15 @@ module.exports = {
     },
     async checkCoupon(couponNumber) {
         header.SOAPAction = api.action + '/GetGiftcardInfo'
-        
+
         let url = api.url + '?op=GetGiftcardInfo'
         let data = {
             couponNumber: encrypt(couponNumber),
         }
         let xml = requestXMLForm('info', data)
-        
+
         consola.info('Send XML Data: (GetGiftcardInfo)', { xml })
-        
+
         try {
             let res = await soap({
                 method: 'POST',
@@ -111,17 +111,17 @@ module.exports = {
                 xml: xml,
                 timeout: 5000,
             })
-            
+
             let parser = new xml2js.Parser()
-            
+
             return new Promise((resolve, reject) => {
-                
+
                 parser.parseString(res.response.body, (err, result) => {
                     if (err) {
                         consola.error('XML Parsing Error', err)
                         reject(err)
                     }
-                    
+
                     let envelope = Object.values(result)
                     let body = Object.values(envelope[0])
                     let giftCardInfo = body[1][0].GetGiftcardInfoResponse
@@ -132,7 +132,7 @@ module.exports = {
                         resolve('SERVERERROR')
                     }
                     let GfctInfo = giftCardResult[0].GfctInfo[0]
-                    
+
                     if (apiResult.$.ResultCd !== '0000') {
                         consola.error(`COOP ERROR: CODE ${apiResult.$.ResultCd}`)
                         resolve('MISMATCH')
@@ -141,7 +141,7 @@ module.exports = {
                         consola.error('COOP ERROR: Coupon Inactive')
                         resolve('INACTIVE')
                     }
-                    
+
                     resolve({
                         couponNumber: couponNumber,
                         price: GfctInfo.$.RmndAmt,
@@ -155,12 +155,12 @@ module.exports = {
     },
     async useCancel(data) {
         header.SOAPAction = api.action + '/UseCancel'
-        
+
         let url = api.url + '?op=UseCancel'
         data.couponNumber = encrypt(data.couponNumber)
-        
+
         let xml = requestXMLForm('cancel', data)
-        
+
         try {
             let res = await soap({
                 method: 'POST',
@@ -169,29 +169,29 @@ module.exports = {
                 xml: xml,
                 timeout: 5000,
             })
-            
+
             let parser = new xml2js.Parser()
-            
+
             return new Promise((resolve, reject) => {
-                
+
                 parser.parseString(res.response.body, (err, result) => {
                     if (err) {
                         consola.error('XML Parsing Error', err)
                         reject(err)
                     }
-                    
+
                     let envelope = Object.values(result)
                     let body = Object.values(envelope[0])
                     let info = body[1][0].UseCancelResponse
                     let _result = info[0].UseCancelResult
                     let apiResult = _result[0].ApiResult[0]
-                    
+
                     if (!_result[0].AprvResult) {
                         resolve('SERVERERROR')
                     }
-                    
+
                     let aprvResult = _result[0].AprvResult[0]
-                    
+
                     if (apiResult.$.ResultCd !== '0000') {
                         consola.error(`COOP ERROR CODE : ${apiResult.$.ResultCd}`)
                         resolve('MISMATCH')
@@ -200,7 +200,7 @@ module.exports = {
                         consola.error('COOP Coupon Inactive')
                         resolve('INACTIVE')
                     }
-                    
+
                     resolve('SUCCESS')
                 })
             })
@@ -211,12 +211,12 @@ module.exports = {
     },
     async useNetworkCancel(data) {
         header.SOAPAction = api.action + '/UseNetworkCancel'
-        
+
         let url = api.url + '?op=UseNetworkCancel'
         data.couponNumber = encrypt(data.couponNumber)
-        
+
         let xml = requestXMLForm('netCancel', data)
-        
+
         try {
             let res = await soap({
                 method: 'POST',
@@ -225,29 +225,29 @@ module.exports = {
                 xml: xml,
                 timeout: 5000,
             })
-            
+
             let parser = new xml2js.Parser()
-            
+
             return new Promise((resolve, reject) => {
-                
+
                 parser.parseString(res.response.body, (err, result) => {
                     if (err) {
                         consola.error('XML Parsing Error', err)
                         reject(err)
                     }
-                    
+
                     let envelope = Object.values(result)
                     let body = Object.values(envelope[0])
                     let info = body[1][0].UseNetworkCancelResponse
                     let _result = info[0].UseNetworkCancelResult
                     let apiResult = _result[0].ApiResult[0]
-                    
+
                     if (!_result[0].AprvResult) {
                         resolve('SERVERERROR')
                     }
-                    
+
                     let aprvResult = _result[0].AprvResult[0]
-                    
+
                     if (apiResult.$.ResultCd !== '0000') {
                         consola.error(`COOP ERROR CODE : ${apiResult.$.ResultCd}`)
                         resolve('MISMATCH')
@@ -256,7 +256,7 @@ module.exports = {
                         consola.error('COOP Coupon Inactive')
                         resolve('INACTIVE')
                     }
-                    
+
                     resolve('SUCCESS')
                 })
             })
@@ -273,10 +273,10 @@ function encrypt(value) {
     let iv = Buffer.from(api.iv, 'hex')
     let cipher = crypto.createCipheriv(algorithm, key, iv)
     cipher.setAutoPadding(false)
-    
+
     let result = cipher.update(pkcs7Pad(value), undefined, 'base64')
     result += cipher.final('base64')
-    
+
     return result
 }
 
@@ -286,42 +286,42 @@ function decrypt(value) {
     let iv = Buffer.from(api.iv, 'hex')
     let decipher = crypto.createDecipheriv(algorithm, key, iv)
     decipher.setAutoPadding(false)
-    
+
     let result = decipher.update(value, 'base64', 'utf8')
     result += decipher.final('utf8')
-    
+
     return pkcs7Unpad(result)
 }
 
 const pkcs7Pad = (params) => {
     const buffer = Buffer.from(params, 'utf8')
     const bytes = new Uint8Array(buffer.length)
-    
+
     let i = buffer.length
-    
+
     while (i--) {
         bytes[i] = buffer[i]
     }
-    
+
     return Buffer.from(pkcs7.pad(bytes))
 }
 
 const pkcs7Unpad = (params) => {
     const buffer = Buffer.from(params, 'utf8')
     const bytes = new Uint8Array(buffer.length)
-    
+
     let i = buffer.length
-    
+
     while (i--) {
         bytes[i] = buffer[i]
     }
-    
+
     return Buffer.from(pkcs7.unpad(bytes)).toString('utf8')
 }
 
 function requestXMLForm(type, data) {
     let request
-    
+
     switch (type) {
         case 'info':
             request =
@@ -409,7 +409,7 @@ function requestXMLForm(type, data) {
                 '</UseCancel>'
             break
     }
-    
+
     return `<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">` +
         ` <soap:Body>` +
         request +
