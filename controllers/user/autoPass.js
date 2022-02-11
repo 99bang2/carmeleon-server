76cd6aa25c8a82b4c313a5a104d0badda356ee2b
jjg0954 <jjg0954@humaxparcs.com>
2022-02-11 11:08:56 +0900

    자동결제차량 UID 가져오는 부분 수정

--- controllers/user/autoPass.js
+++ controllers/user/autoPass.js
@@ -8,20 +8,27 @@ exports.check = async function (ctx) {
     let _ = ctx.request.body
     let parkingSite = await models.parkingSite.findOne({
         where: {
-            autoPassCode : _.autoPassCode
+            autoPassCode: _.autoPassCode
         }
     })
-    if(!parkingSite){
-        response.badRequest()
+    if (!parkingSite) {
+        response.send(ctx, {
+            data: false,
+            message: "자동결제가 가능한 주차장이 아닙니다."
+        })
     }
     let car = await models.car.findOne({
         where: {
-            carPlate: _.carPlate
+            carPlate: _.carPlate,
+            isAutoPass: true
         }
     })
 
-    if(!car.isAutoPass) {
-        response.badRequest()
+    if (!car) {
+        response.send(ctx, {
+            data: false,
+            message: '자동결제 등록된 차량이 없습니다.'
+        })
     }
     let user = await models.user.findOne({
         include: [
@@ -36,18 +43,24 @@ exports.check = async function (ctx) {
             uid: car.userUid
         }
     })
-    if(!user.isAutoPass){
-        response.badRequest()
+    if (!user.isAutoPass) {
+        response.send(ctx, {
+            data: false,
+            message: '자동결제를 등록한 사용자가 아닙니다.'
+        })
     }
 
-    let filteredCards =user.cards.filter((card) => card.isAutoPass)
+    let filteredCards = user.cards.filter((card) => card.isAutoPass)
     let card = filteredCards[0]
-    if(!card.isAutoPass){
-        response.badRequest()
+    if (!card.isAutoPass) {
+        response.send(ctx, {
+            data: false,
+            message: '자동결제 등록된 카드가 없습니다.'
+        })
     }
-    if(_.type==='in'){
+    if (_.type === 'in') {
         models.push.create({
-            pushType : 1,
+            pushType: 1,
             title: '차량 입차 완료',
             body: `${car.carPlate} 차량이 ${parkingSite.name}에 입차했습니다.`,
             userToken: user.token,
@@ -60,7 +73,7 @@ exports.check = async function (ctx) {
 exports.enableAutoPassList = async function (ctx) {
     let parkingList = await models.parkingSite.findAll({
         where: {
-            autoPassCode : {
+            autoPassCode: {
                 [Op.ne]: null
             }
         }
@@ -68,7 +81,7 @@ exports.enableAutoPassList = async function (ctx) {
     response.send(ctx, {data: parkingList})
 }
 
-exports.getAutoPass = async function(ctx) {
+exports.getAutoPass = async function (ctx) {
     let {uid} = ctx.params
     let user = await models.user.findOne({
         include: [
@@ -83,12 +96,12 @@ exports.getAutoPass = async function(ctx) {
             uid: uid
         }
     })
-    let filteredCards =user.cards.filter((card) => card.isAutoPass)
+    let filteredCards = user.cards.filter((card) => card.isAutoPass)
     let card = filteredCards[0]
-    let filteredCars =user.cars.filter((car) => car.isAutoPass)
+    let filteredCars = user.cars.filter((car) => car.isAutoPass)
     let car = filteredCars[0]
 
-    response.send(ctx, {user,car,card})
+    response.send(ctx, {user, car, card})
 }
 exports.requestPayment = async function (ctx) {
     let _ = ctx.request.body
@@ -96,7 +109,7 @@ exports.requestPayment = async function (ctx) {
     let carPlate = _.carPlate
     let parkingSite = await models.parkingSite.findOne({
         where: {
-            autoPassCode : {
+            autoPassCode: {
                 [Op.ne]: null
             }
         }
@@ -115,37 +128,37 @@ exports.requestPayment = async function (ctx) {
             uid: _.uid
         }
     })
-    let filteredCards =user.cards.filter((card) => card.isAutoPass)
+    let filteredCards = user.cards.filter((card) => card.isAutoPass)
     let card = filteredCards[0]
     // 결제
 
     // 자동결제 요청
 
-    let totalPrice 		= Number(_.price)
+    let totalPrice = Number(_.price)
 
     // 가격 확인
-    if(totalPrice !== Number(_.price)) {
+    if (totalPrice !== Number(_.price)) {
         response.customError(ctx, '잘못된 요금입니다.')
     }
     // 데이터 생성 (LOCK)
     let payLog = await models.payLog.create({
-        carNumber			: carPlate,
-        reserveTime			: '',
-        payType				: 'autoPass',
-        siteUid				: parkingSite.uid,
-        cardUid				: card.uid,
-        discountTicketUid	: null,
-        status				: 10,
-        sellingPrice		: 0,
-        discountPrice		: 0,
-        price				: price,
-        totalPrice			: totalPrice,
-        userUid				: user.uid,
-        phoneNumber			: user.phone || '01000000000',
-        email				: user.email || 'mobilx.carmeleon@gmail.com',
-        activeStatus		: false,
-        cancelStatus		: -1,
-        expired				: false
+        carNumber: carPlate,
+        reserveTime: '',
+        payType: 'autoPass',
+        siteUid: parkingSite.uid,
+        cardUid: card.uid,
+        discountTicketUid: null,
+        status: 10,
+        sellingPrice: 0,
+        discountPrice: 0,
+        price: price,
+        totalPrice: totalPrice,
+        userUid: user.uid,
+        phoneNumber: user.phone || '01000000000',
+        email: user.email || 'mobilx.carmeleon@gmail.com',
+        activeStatus: false,
+        cancelStatus: -1,
+        expired: false
     })
     // 결제금액이 있을 경우 결제 처리.
     if (totalPrice && totalPrice > 0) {
@@ -154,11 +167,11 @@ exports.requestPayment = async function (ctx) {
             billKey: card.billKey,
             price: totalPrice,
             goodsName: '자동결제',
-            buyerName:  '사용자',
+            buyerName: '사용자',
             buyerEmail: payLog.email || 'mobilx.carmeleon@gmail.com',
             buyerTel: payLog.phoneNumber
         })
-        if(payResult.resultCode !== '3001') {
+        if (payResult.resultCode !== '3001') {
             payLog.status = -10
             await payLog.save()
             response.customError(ctx, '[결제실패] ' + payResult.resultMsg)
@@ -168,12 +181,12 @@ exports.requestPayment = async function (ctx) {
         payLog.payTid = payResult.tid
     }
     await models.push.create({
-        pushType 	: 1,
-        title		: '자동정산이 완료되었습니다.',
-        body		: `결제된 금액은 총 ${totalPrice}원 입니다.`,
-        userToken	: user.token,
-        userUid		: user.uid,
-        sendDate	: Sequelize.fn('NOW')
+        pushType: 1,
+        title: '자동정산이 완료되었습니다.',
+        body: `결제된 금액은 총 ${totalPrice}원 입니다.`,
+        userToken: user.token,
+        userUid: user.uid,
+        sendDate: Sequelize.fn('NOW')
     })
     payLog.status = 10
     payLog.activeStatus = true
@@ -181,41 +194,41 @@ exports.requestPayment = async function (ctx) {
     response.send(ctx, payLog)
 
 }
-exports.registerIsAutoPass = async function(ctx) {
+exports.registerIsAutoPass = async function (ctx) {
     let _ = ctx.request.body
     let user = await models.user.getByUid(ctx, _.userUid)
-    let car =  await models.car.getByUid(ctx, _.carUid)
-    let card = await models.card.getByUid(ctx,_.cardUid)
+    let car = await models.car.getByUid(ctx, _.carUid)
+    let card = await models.card.getByUid(ctx, _.cardUid)
     user.isAutoPass = true
     car.isAutoPass = true
     card.isAutoPass = true
     await user.save()
     await car.save()
     await card.save()
-    response.send(ctx, {user,car,card})
+    response.send(ctx, {user, car, card})
 }
-exports.terminateIsAutoPass = async function(ctx) {
+exports.terminateIsAutoPass = async function (ctx) {
     let _ = ctx.request.body
     let user = await models.user.getByUid(ctx, _.userUid)
-    let car =  await models.car.getByUid(ctx, _.carUid)
-    let card = await models.card.getByUid(ctx,_.cardUid)
+    let car = await models.car.getByUid(ctx, _.carUid)
+    let card = await models.card.getByUid(ctx, _.cardUid)
     user.isAutoPass = false
     car.isAutoPass = false
     card.isAutoPass = false
     await user.save()
     await car.save()
     await card.save()
-    response.send(ctx, {user,car,card})
+    response.send(ctx, {user, car, card})
 }
 
-exports.changeIsRead = async function(ctx) {
+exports.changeIsRead = async function (ctx) {
     let {uid} = ctx.params
     let payLogs = await models.payLog.findAll({
         where: {
             userUid: uid,
         }
     })
-    for(let payLog of payLogs) {
+    for (let payLog of payLogs) {
         let autoPassLog = await models.payLog.findByPk(payLog.uid)
         autoPassLog.isRead = true
         await autoPassLog.save()
@@ -223,7 +236,7 @@ exports.changeIsRead = async function(ctx) {
     response.send(ctx, payLogs)
 }
 
-exports.checkCarAutoPass = async function(ctx) {
+exports.checkCarAutoPass = async function (ctx) {
     let _ = ctx.request.body
     let car = await models.car.findOne({
         where: {
@@ -231,12 +244,12 @@ exports.checkCarAutoPass = async function(ctx) {
             isAutoPass: true
         }
     })
-    if(car) {
+    if (car) {
         response.send(ctx, {
             data: false,
             message: "이미 등록된 차량번호 입니다."
         })
-    }else{
+    } else {
         response.send(ctx, {
             data: true,
             message: "등록이 가능합니다."
